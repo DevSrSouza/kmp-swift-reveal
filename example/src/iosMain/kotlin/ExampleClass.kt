66dfded0592dc5e9d618b5c7b1f481d2e83db839
DevSrSouza <devsrsouza@gmail.com>
2023-10-12 17:50:37 -0300

    chore: swift reveal workflow with diffing example

--- example/src/iosMain/kotlin/ExampleClass.kt
+++ example/src/iosMain/kotlin/ExampleClass.kt
@@ -1,6 +1,8 @@
 import kotlin.experimental.ExperimentalObjCName
 
 class ExampleClass {
+    val someProperty = "String property"
+
     fun mySuperFun(): Int = 10
     fun withCallback(myCallback: () -> String) {}
     fun myFunctionReturnMap(): Map<String, String> = mapOf("Example" to "Value")
@@ -8,8 +10,13 @@ class ExampleClass {
     @OptIn(ExperimentalObjCName::class)
     @ObjCName(swiftName = "mySwiftNameFunction")
     fun withObjcName() {}
+
+    @OptIn(ExperimentalObjCName::class)
+    @ObjCName(swiftName = "swiftDoSomethingReallyCool")
+    fun doSomethingReallyCool() {}
 }
 
 fun ExampleClass.myExtensionFunction() {}
+fun ExampleClass.newExtension() {}
 
 fun myTopLevelFunction(): String = "Example return string"
